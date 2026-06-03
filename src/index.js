dcae1fbd4b5d191489041701059b6c1907077cdd
sxmurxi35 <sxmurxin@proton.me>
2026-06-03 16:42:03 +0200

    Added working switch for changing units

--- src/index.js
+++ src/index.js
@@ -3,12 +3,32 @@ import getWeather from "./script/apiCall.js";
 
 getWeather("chwaliszow", true);
 
+let unit = true;
+
 const searchButton = document.querySelector("#search-button");
 searchButton.addEventListener("click", () => {
   const input = document.querySelector("#cityInput");
   const city = input.value;
 
-  getWeather(city, true);
+  getWeather(city, unit);
   input.setAttribute("placeholder", city);
   input.value = "";
+});
+
+const unitToggle = document.querySelector("#unitSwitch");
+
+window.addEventListener("load", () => {
+  unitToggle.checked = false;
+});
+
+unitToggle.addEventListener("change", () => {
+  if (unitToggle.checked) {
+    unit = false;
+    document.querySelector(".celsius").classList.toggle("selected");
+    document.querySelector(".fahrenheit").classList.toggle("selected");
+  } else if (!unitToggle.checked) {
+    unit = true;
+    document.querySelector(".celsius").classList.toggle("selected");
+    document.querySelector(".fahrenheit").classList.toggle("selected");
+  }
 });
